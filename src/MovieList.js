d0c056a7c62493d5fc13e5e0e9e7f25f72fda2e9
alin-ionut <ionutghidescu@gmail.com>
2018-08-22 20:26:41 +0300

    movie-card

--- src/MovieList.js
+++ src/MovieList.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
+import MovieItem from './MovieItem';
+import styles from './MovieList.css';
 
 function MovieList(){
     return (
-        <div>
+        
+        <div className="movie-list">
+            <div className="movies-container">
+                <MovieItem 
+                title="Independence Day"
+                year="1996"
+                description="The aliens are coming and their goal is to invade and destroy Earth. Fighting superior technology, mankind's best weapon is the will to survive."/>
+            </div>
             <Button variant="contained" color="primary" >
             Add new movie
             </Button>
