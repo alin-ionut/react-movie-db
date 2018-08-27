3fc61cea57bbc5319eff93343318ebc106f04efc
alin-ionut <ionutghidescu@gmail.com>
2018-08-27 20:45:03 +0300

    changes_27_08

--- src/MovieList.js
+++ src/MovieList.js
@@ -3,15 +3,21 @@ import Button from '@material-ui/core/Button';
 import MovieItem from './MovieItem';
 import styles from './MovieList.css';
 
-function MovieList(){
+function MovieList(props){
+    const { movies = [] } = props;
     return (
-        
         <div className="movie-list">
             <div className="movies-container">
-                <MovieItem 
-                title="Independence Day"
-                year="1996"
-                description="The aliens are coming and their goal is to invade and destroy Earth. Fighting superior technology, mankind's best weapon is the will to survive."/>
+            {movies.length > 0 ?
+             movies.map(movie => (
+                <MovieItem
+                title={movie.name}
+                year={movie.year}
+                description={movie.description} 
+                key={movie.id}
+                />
+    ))
+    : 'No movies found. Perhaps add one?'}
             </div>
             <Button variant="contained" color="primary" >
             Add new movie
